8f9f7002c1d55816d0d17c5a050bae1ab01f61ff
Ashwin Oke <okeashwin@gmail.com>
2015-09-15 20:37:03 -0400

    HW1 Updated

--- HW1/awsClient.py
+++ HW1/awsClient.py
@@ -9,7 +9,7 @@ for instance in ec2.get_all_instances():
     if instance.id == image_instance.id:
         break
 with open('inventory', 'a') as file:
-    file.write('node1 ansible_ssh_host='+str(instance.instances[0].ip_address)+' ansible_ssh_user=ubuntu ansible_ssh_private_key_file=sample_key_east.pem\n')
+    file.write('node1 ansible_ssh_host='+str(instance.instances[0].ip_address)+' ansible_ssh_user=ubuntu ansible_ssh_private_key_file=~/sample_key_east.pem\n')
 
 print("Provisioned a server on AWS with IP : "+str(instance.instances[0].ip_address))
 print("Instance successfully spun up. Added entry for this server in the inventory file")
